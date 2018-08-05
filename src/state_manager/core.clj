202cd3cb4097c404aa97190b8244b87c255c8997
peterjewicz <peterjewicz@totalwebconnections.com>
2018-08-05 09:29:34 -0500

    update reset to take initial values

--- src/state_manager/core.clj
+++ src/state_manager/core.clj
@@ -8,25 +8,24 @@
   [state]
   (swap! app-state merge state))
 
-(defn state-reset
-  "Multi arity, can reset the state to blank, or to a default value passed in"
-  []
-  (reset! app-state {}))
-
-(defn get-state
-  "This should be a multi arity function that when passed a key returns just that or else returns the whole state"
-  ([] @app-state)
-  ([key] (key @app-state))
-  )
-
 (defn set-initial-state
   "Should be used to set initial state, completly swaps out the state"
   [state]
   (swap! app-state merge state))
 
+(defn state-reset
+  "Multi arity, can reset the state to blank, or to a default value passed in"
+  ([](reset! app-state {}))
+  ([state] (set-initial-state state)))
+
+(defn get-state
+  "This should be a multi arity function that when passed a key returns just that or else returns the whole state"
+  ([] @app-state)
+  ([key] (key @app-state)))
+
 
 ;examples
-;(set-initial-state {:test 5 :test2 10})
+; (set-initial-state {:test 5 :test2 10})
 ;(update-state {:test 5 :test2 2 :test3 40})
 ; (get-state)
 ; (get-state :test)
